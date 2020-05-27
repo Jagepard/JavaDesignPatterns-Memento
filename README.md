8373e429276f289cc5b4239171c6cb77d1b1a348
Jagepard <dankorot@gmail.com>
2020-05-27 18:26:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaDesignPatterns-Memento
+
+## Result:
+```
+```
---
+++ src/Behavioral/Memento/Main.java
@@ -0,0 +1,13 @@
+/*
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
+ */
+
+package Behavioral.Memento;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
