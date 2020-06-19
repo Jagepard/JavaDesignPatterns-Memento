c345e15cf954a590a826815f32d5045f25e5ee05
Jagepard <dankorot@gmail.com>
2020-06-19 13:00:54 +0300

    Add Serialization, De-serialization to save

--- src/Behavioral/Memento/Caretaker.java
+++ src/Behavioral/Memento/Caretaker.java
@@ -5,6 +5,7 @@
 
 package Behavioral.Memento;
 
+import java.io.*;
 import java.util.LinkedList;
 
 public class Caretaker implements CaretakerInterface {
@@ -14,7 +15,17 @@ public class Caretaker implements CaretakerInterface {
         return this.history.removeLast();
     }
 
-    public void save(MementoInterface memento) {
-        this.history.add(memento);
+    public void save(MementoInterface memento) throws Exception
+    {
+        //Serialization of object
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(bos);
+        out.writeObject(memento);
+        out.close();
+        //De-serialization of object
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream in = new ObjectInputStream(bis);
+        this.history.add((MementoInterface) in.readObject());
+        in.close();
     }
 }
